70d899d40631f21537ecde1ff2c17ff80819b540
Javier Sandino <javier@sandino.ca>
2024-08-02 19:28:12 -0400

    Parsing emotion_detector results.

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,18 @@
 import requests
+import json
+from collections import Counter
 
 def emotion_detector(text_to_analyze):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     HEADERS = { "grpc-metadata-mm-model-id" : "emotion_aggregated-workflow_lang_en_stock" }
     json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL, headers=HEADERS, json=json)
-    return response.text
+    result = parse_emotions(response.text)
+    return result
+
+def parse_emotions(text: str) -> dict[str, any]:
+    data = json.loads(text)
+    emotions = data["emotionPredictions"][0]["emotion"]
+    dominant, _ = Counter(emotions).most_common(1)[0]
+    emotions.update({'dominant_emotion' : dominant})
+    return emotions
---
+++ test_emotion_detection.py
@@ -0,0 +1,31 @@
+import pytest
+from emotion_detection import parse_emotions, emotion_detector
+
+@pytest.fixture
+def sample_data() -> str:
+ return '{"emotionPredictions":[{"emotion":{"anger":0.0132405795, "disgust":0.0020517302, "fear":0.009090992, "joy":0.9699522, "sadness":0.054984167}, "target":"", "emotionMentions":[{"span":{"begin":0, "end":26, "text":"I love this new technology"}, "emotion":{"anger":0.0132405795, "disgust":0.0020517302, "fear":0.009090992, "joy":0.9699522, "sadness":0.054984167}}]}], "producerId":{"name":"Ensemble Aggregated Emotion Workflow", "version":"0.0.1"}}'
+
+@pytest.fixture
+def expected_parsed_data() -> dict[str, any]:
+    return {
+        'anger' : 0.0132405795,
+        'disgust' : 0.0020517302,
+        'fear' : 0.009090992,
+        'joy' : 0.9699522,
+        'sadness' : 0.054984167,
+        'dominant_emotion': 'joy'
+    }
+
+@pytest.fixture
+def expected_emotion_keys() -> tuple[str]:
+    return ('anger', 'disgust', 'fear', 'joy', 'sadness', 'dominant_emotion')
+
+def test_parse_emotions(sample_data, expected_parsed_data):
+    assert expected_parsed_data == parse_emotions(sample_data)
+
+def test_emotion_detector_results(expected_emotion_keys):
+    result = emotion_detector("I love this technology")
+    for emotion_key in expected_emotion_keys:
+        assert emotion_key in result
+    
+
