f2a8ed46b78915a208b4fe4b065e545ad9748e36
Javier Sandino <javier@sandino.ca>
2024-08-02 22:47:14 -0400

    Static code analysis improvements.

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+A REST API to detect emotions from text.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,11 +9,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    Default endpoint rendering an input form to capture text to be analyzed.
+    """
     return render_template("index.html")
 
 
 @app.route("/emotionDetector")
 def detect_emotion():
+    """
+    Endpoint performing the text analysis to yield a normal distribution of emotions.
+    """
     text = request.args["textToAnalyze"]
     result = emotion_detector(text)
     if not result["dominant_emotion"]:
@@ -20,10 +29,14 @@ def detect_emotion():
 
 
 def summarize(data: dict[str, any]):
+    """
+    Summarizes the text classification results in a human-friendly format.
+    """
     emotions = [f"'{k}': {v}" for k, v in data.items() if k != 'dominant_emotion']
-    emotions_text = ", ".join(emotions) 
+    emotions_text = ", ".join(emotions)
     dominant = data["dominant_emotion"]
-    return f'For the given statement, the system response is {emotions_text}. The dominant emotion is <b>{dominant}</b>.'
+    return (f"For the given statement, the system response is {emotions_text}. "
+            f"The dominant emotion is <b>{dominant}</b>.")
 
 
 if __name__ == "__main__":
